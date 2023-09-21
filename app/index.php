a3475d6176d7325553d58ce798dd6cf1e5ccb47b
«sara21h» <sbogdan@iesebre.com>
2023-09-21 19:10:39 +0200

    3r commit- MVC

--- app/index.php
+++ app/index.php
@@ -1,3 +1,19 @@
 <?php
 
-$greeting = 'Hola mon!';
+function greet(){
+    $name = htmlspecialchars($_GET['name']);
+    $surname = $_GET['surname'];
+
+    return "Hola $name $surname!";
+}
+//executem funció
+$greeting = greet();
+
+//QUERY STRING
+// imprimeix arrays (chivato) -> var_dump($_GET); de normal echo
+//var_dump($_GET['name']);
+//$greeting = 'Hola ' . $_GET['name'] . ' ' . $_GET['cognom'] . '!';
+//"" quan posem variables, les simples no van amb variables
+// o $greeting = 'Hola' . ${name} . '!';
+//{} per a variables amb mes d1 frase
+
