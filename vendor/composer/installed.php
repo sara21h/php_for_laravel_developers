087ef1c1c1f425353217899f82ee29418118275f
«sara21h» <sbogdan@iesebre.com>
2023-10-05 20:09:32 +0200

    Canvis del main al master

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sara/php_for_laravel_developers',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ed0854089644989002c79b9f0ab6f7494ede93b5',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sara/php_for_laravel_developers' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ed0854089644989002c79b9f0ab6f7494ede93b5',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
